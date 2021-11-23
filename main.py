3d94df5f7b927e34ae4a5d62d5ed5c3a47aedfd3
GGggg-sp <94873923+GGggg-sp@users.noreply.github.com>
2021-11-23 20:31:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ import pickle as pkl
 from openpyxl import Workbook
 from openpyxl.styles import Font, Color, Alignment, Border, Side, PatternFill
 
+# The number of nodes on the map
+number_of_nodes = 92
 
 # The tools collection list
 tools_list = ['手', '藤条', '小红', '小绿', '树脂棍', '皮带', '戒尺']
@@ -50,15 +52,21 @@ possibilities_base_list = [0.15, 0.05, 0.7, 0.1]
 # not sp event list contains all the event types that are not spanking or special game patterns in 404 sanctuary
 not_sp_event_list = ['揉揉', '晾臀', '后退一格', '后退两格', '回到起点', '前进一格', '前进两格', '请主随意']
 # special events featured by some dalaos in 404 sanctuary
-special_event_list = ['地刺王冰冰出现了！', 'Genecro 请您回到起点并请罚', 'JayZ 为您点了数量任意的藤条',
-                      'Zara 团宠觉得您应该享受更多的小红', '礼物姐看了眼你的屁屁表示不屑', '等子姐在等您前往避难所',
-                      '泰坦：您今日戒色了吗？', '冷子姐请您喝奶啤哦', '小诺姐值班时看到了您并将您写进了小说',
-                      'linu 告诉你反被为主并不是好的选择', 'ladboy 的藤条到货了想请您去品尝', '悠久のSP資料館欢迎您前来阅览',
-                      '火球太太声称火球在拍灰，火球对此事并没有回应', 'vv 最近失踪辣，可能是在躲着姐姐叭',
-                      '你这个一点都不重度诶，拳王如实说道', 'Netspanker对你主的手法表示质疑','dokoham 永远在默默无闻的搬运着资源']
+special_event_list = ['地刺王冰冰出现了！请您后退六格！', 'Genecro 请您回到起点并请罚', 'JayZ 为您点了数量任意的藤条',
+                      'Zara 团宠觉得您应该享受更多的小红', '礼物姐看了眼你的屁屁表示不屑并建议增加任意数量的巴掌',
+                      '等子姐在等您前往避难所，因此您增加了晾臀时间',
+                      '泰坦：您今日戒色了吗？请您根据主的要求 DIY ', '冷子姐请您喝奶啤哦！喝完增加任意数量的棍状工具',
+                      '小诺姐值班时看到了您并将您写进了小说，本轮您可以自由选择将受到的惩罚',
+                      'linu 告诉您反被为主并不是好的选择，并为您增加了亿些皮带数目', 'ladboy 的藤条到货了想请您去品尝，因此您增加了任意数量的藤条！',
+                      '悠久のSP資料館欢迎您前来阅览，您获得了五分钟的休息时间！',
+                      '火球太太声称火球在拍灰，火球对此事并没有回应，因此太太决定为您增加重度戒尺',
+                      'vv 最近失踪辣，可能是在躲着姐姐叭，请您帮助 vv 承担亿些 OTK 手掌',
+                      '你这个一点都不重度诶，拳王如实说道，并为您点了重度藤条',
+                      'Netspanker对你主的手法表示质疑，您有一次机会可以反被为主，冲！',
+                      'dokoham 永远在默默无闻的搬运着资源，您选择挨任意数量的板状工具支持他']
 special_event_list_bac = special_event_list.copy()
 # possibilities for sp, not sp event and the special event
-possibilities_event_list = [0.7, 0.15, 0.15]
+possibilities_event_list = [0.75, 0.15, 0.1]
 event_list = ['sp', 'not sp', 'special']
 
 num_max_per_epoch = 40
@@ -75,7 +83,7 @@ def generate_single_event(process_p):
         base = np.random.choice(base_list, 1, p=possibilities_base_list).tolist()[0]
         num = str(np.random.choice(num_list, 1).tolist()[0])
 
-        event_str = '姿势：' + position + '\n工具：' + tool + '\n报数进制：' + base + '\n数目：' + str(num)
+        event_str = '姿势：' + position + '\n工具：' + tool + '\n数目：' + str(num) + '\n以' + base + '报数'
         event_dict = {'type': 'spanking', 'position': position, 'tool': tool, 'base': base, 'num': num}
     elif event_type == 'not sp':
         event_str = np.random.choice(not_sp_event_list)
@@ -100,10 +108,10 @@ def save_excel(event_series_str, excel_filename):
     workbook = Workbook()
     sheet = workbook.active
 
-
-    current_max_h = 16
+    # Calculate the size of the ludo map
+    current_max_h = int(np.ceil(np.sqrt(len(event_series_str) * 2)) + 2)
     current_min_h = 1
-    current_max_v = 16
+    current_max_v = current_max_h
     current_min_v = 1
     current_h = 1
     current_v = 1
@@ -113,9 +121,9 @@ def save_excel(event_series_str, excel_filename):
     space = 2
 
     for i in range(1, current_max_v):
-        sheet.row_dimensions[i].height = 70
+        sheet.row_dimensions[i].height = 75
     for i in range(1, current_max_h):
-        sheet.column_dimensions[chr(base_ascii+i)].width = 22
+        sheet.column_dimensions[chr(base_ascii+i)].width = 20
     for ind in range(len(event_series_str)):
         current_h = current_h + speed_h
         current_v = current_v + speed_v
@@ -132,6 +140,8 @@ def save_excel(event_series_str, excel_filename):
         sheet[current_index].alignment = center_aligned_text
         if event_series_str[ind] in special_event_list_bac:
             sheet[current_index].fill = PatternFill(start_color="F48225", fill_type='solid')
+        elif event_series_str[ind] in not_sp_event_list:
+            sheet[current_index].fill = PatternFill(start_color="BBBBAA", fill_type='solid')
         # check if reached the boundary
         # ->
         if current_h + space == current_max_h and speed_h > 0:
@@ -161,7 +171,8 @@ def save_excel(event_series_str, excel_filename):
 
 
 if __name__ == '__main__':
-    event_series_str, event_series_list = generate_event_series(event_max_num=90)
+
+    event_series_str, event_series_list = generate_event_series(event_max_num=number_of_nodes)
     # for i in range(len(event_series_str)):
     #     print('第', i+1, '格')
     #     print(event_series_str[i])
